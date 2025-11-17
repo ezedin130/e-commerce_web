7bf44664c8df21552b4504bad501f6b3ed742313
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-11-17 14:49:00 +0300

    feat: implement onPressed to switch password field between visible and hidden

--- lib/util/reusable_textfield.dart
+++ lib/util/reusable_textfield.dart
@@ -1,30 +1,47 @@
 
 import 'package:flutter/material.dart';
 
-class ReUsableTextfield extends StatelessWidget {
+class ReUsableTextfield extends StatefulWidget {
    ReUsableTextfield({
     super.key,
     required this.text,
     required this.obscure,
     required this.icon,
+     required this.controller,
     this.isPassword = false,
   });
   final String text;
-  final bool obscure;
+   bool obscure;
   final IconData icon;
+  final TextEditingController controller;
   final bool isPassword;
+
+  @override
+  State<ReUsableTextfield> createState() => _ReUsableTextfieldState();
+}
+
+class _ReUsableTextfieldState extends State<ReUsableTextfield> {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      obscureText: obscure,
+      obscureText: widget.obscure,
+      controller: widget.controller,
       decoration:  InputDecoration(
         isDense: true,
-        prefixIcon: Icon(icon),
-        suffixIcon: isPassword ? IconButton(
-            icon: Icon(Icons.visibility_off),
-            onPressed: (){},
-        ): null,
-        hintText: text,
+        prefixIcon: Icon(widget.icon),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+          icon: Icon(
+            widget.obscure ? Icons.visibility_off : Icons.visibility,
+          ),
+          onPressed: () {
+            setState(() {
+              widget.obscure = !widget.obscure;
+            });
+          },
+        )
+            : null,
+        hintText: widget.text,
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
           borderSide: BorderSide(color: Colors.grey, width: 1.0),
