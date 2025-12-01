7c7db15edf0aa6468790caa4fa6b87e58afd04d4
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-02 00:00:05 +0300

    feat: purchase section

---
+++ lib/pages/home/section/purchase_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PurchaseSection extends StatelessWidget {
+  const PurchaseSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          'Purchase Content'
+      ),
+    );
+  }
+}
