6c816227556d990d51baa32ff5ed98bafbab5499
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-01 23:59:58 +0300

    feat: product section

---
+++ lib/pages/home/section/product_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductSection extends StatelessWidget {
+  const ProductSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          'Product Content'
+      ),
+    );
+  }
+}
