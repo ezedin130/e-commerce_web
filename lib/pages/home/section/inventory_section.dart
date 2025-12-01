570661c54ab661a0d02f0b2cf45b6eddfcc1cb98
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-01 23:59:45 +0300

    feat: inventory section

---
+++ lib/pages/home/section/inventory_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InventorySection extends StatelessWidget {
+  const InventorySection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          'Inventory Content'
+      ),
+    );
+  }
+}
