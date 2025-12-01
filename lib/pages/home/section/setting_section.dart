9aa004c6c7bc90ef78ce20703cb17119c73c5b8f
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-02 00:00:14 +0300

    feat: setting section

---
+++ lib/pages/home/section/setting_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SettingSection extends StatelessWidget {
+  const SettingSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          'Setting Content'
+      ),
+    );
+  }
+}
