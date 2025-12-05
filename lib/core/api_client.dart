079401355f3abb748706312d1c83f9bb7175bab0
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-05 16:32:03 +0300

    feat:introduce getAuth method for Bearer token secured endpoints

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiClient{
-  static const String baseUrl = 'http://192.168.1.2:8080';
+  static const String baseUrl = 'http://:8080';
 
   static Map<String, String> headers = {
     'Content-Type': 'application/json',
@@ -11,5 +12,16 @@ class ApiClient{
   }
   static Future<http.Response> login(String endpoint, String body)async{
     return await http.post(uri(endpoint), body: body, headers: headers);
+  }
+  static Future<http.Response> getAuth(String endpoint) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("jwt");
+
+    final authHeaders = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    return await http.get(uri(endpoint), headers: authHeaders);
   }
 }
