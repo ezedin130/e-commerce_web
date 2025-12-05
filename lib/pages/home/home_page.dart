701d7ecf11eca20a861ad1fe70c162d1952e7fed
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-05 16:33:50 +0300

    feat: load username and store name on HomePage from secured endpoints

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,15 @@
+import 'package:e_commerce_mobile/pages/home/section/dashboard/dashboard_section.dart';
+import 'package:e_commerce_mobile/pages/home/section/inventory_section.dart';
+import 'package:e_commerce_mobile/pages/home/section/order_section.dart';
+import 'package:e_commerce_mobile/pages/home/section/product_section.dart';
+import 'package:e_commerce_mobile/pages/home/section/purchase_section.dart';
+import 'package:e_commerce_mobile/pages/home/section/setting_section.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../service/store_service.dart';
 import '../../util/sidebar.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,21 +18,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String name = "";
+  String storeName = "";
   String selectedItem = 'Dashboard';
+
+  final storeService = StoreService();
+  @override
+  void initState() {
+    super.initState();
+    loadNameFromToken();
+    loadStore();
+  }
+
+  Future<String> getUserNameFromToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("jwt");
+    if (token == null) return "User";
+
+    final decodedToken = JwtDecoder.decode(token);
+    return decodedToken['sub'] ?? "User";
+  }
+  void loadNameFromToken() async {
+    final userName = await getUserNameFromToken();
+    setState(() {
+      name = userName;
+    });
+  }
+  void loadStore() async {
+    final store = await storeService.getStoreById(1);
+
+    if (store != null) {
+      setState(() {
+        storeName = store["name"];
+      });
+    }
+  }
+
   Widget getMainContent() {
     switch (selectedItem) {
       case 'Dashboard':
-        return Center(child: Text('Dashboard Content'));
+        return Center(child: DashboardSection());
       case 'Products':
-        return Center(child: Text('Products Content'));
+        return Center(child: ProductSection());
       case 'Inventory':
-        return Center(child: Text('Inventory Content'));
+        return Center(child: InventorySection());
       case 'Orders':
-        return Center(child: Text('Orders Content'));
+        return Center(child: OrderSection());
       case 'Purchases':
-        return Center(child: Text('Purchases Content'));
+        return Center(child: PurchaseSection());
       case 'Settings':
-        return Center(child: Text('Settings Content'));
+        return Center(child: SettingSection());
       default:
         return Center(child: Text('Main Content'));
     }
@@ -117,17 +161,15 @@ class _HomePageState extends State<HomePage> {
                       SizedBox(width: 24),
                       Icon(Icons.notifications_none),
                       SizedBox(width: 16),
-                      Icon(Icons.brightness_2),
-                      SizedBox(width: 16),
                       CircleAvatar(
                         backgroundImage: NetworkImage(
-                            'https://via.placeholder.com/150'),
+                            'https://www.freeiconspng.com/img/25500'),
                       ),
                       SizedBox(width: 5),
                       Column(
                         children: [
                           Text(
-                            'User Name',
+                            name,
                             style: GoogleFonts.lato(
                               fontSize: 15,
                               fontWeight: FontWeight.bold,
@@ -135,7 +177,7 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                           Text(
-                            'Store Name',
+                            storeName,
                             style: GoogleFonts.lato(
                               fontSize: 10,
                               fontWeight: FontWeight.bold,
@@ -168,4 +210,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
